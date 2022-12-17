21a98d2fc9a145799362d422b4b7afa27be05c1c
Matt Bruer <mbruer@gmail.com>
2022-12-16 20:20:13 -0600

    initial commit

---
+++ client/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
+// import history from './history';
+// import store from './store';
+import App from './App';
+
+const container = document.getElementById('app');
+
+const root = createRoot(container);
+root.render(<App tab="home" />);

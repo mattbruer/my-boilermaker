7fa109cb01a1288841b8cf3c42fa83074c8833d6
Matt Bruer <mbruer@gmail.com>
2023-02-13 19:49:52 -0600

    rearranged server app.js

--- server/app.js
+++ server/app.js
@@ -18,6 +18,16 @@ app.use("/api", require("./api"));
 // static file-serving middleware
 app.use(express.static(path.join(__dirname, "..", "public")));
 
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public/index.html"));
+});
+
+// error handling endware
+app.use((err, req, res, next) => {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || "Internal server error.");
+});
 // any remaining requests with an extension (.js, .css, etc.) send 404
 app.use((req, res, next) => {
   if (path.extname(req.path).length) {
@@ -33,13 +43,3 @@ app.use((req, res, next) => {
 
 const PORT = process.env.PORT || 8080;
 // sends index.html
-app.use("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "public/index.html"));
-});
-
-// error handling endware
-app.use((err, req, res, next) => {
-  console.error(err);
-  console.error(err.stack);
-  res.status(err.status || 500).send(err.message || "Internal server error.");
-});
